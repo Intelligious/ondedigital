917b17398eece9a1d5aeb3a3e2775ceeeb7f71bb
Intelligious <m.muneebale@gmail.com>
2023-07-17 11:10:33 +0500

    netlify update

--- pages/_app.js
+++ pages/_app.js
@@ -9,6 +9,7 @@ function MyApp({ Component, pageProps }) {
     <>
       <Head>
         <title>öndë digital - custom software development company in Australia</title>
+        <link rel="icon" href="images/onde digital1.svg" type="image/png"></link>
       </Head>
       <Component {...pageProps} />
       <Script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" />
